6741deb8169e04e4b6d6dc2100b83ae864e7d77e
haulth <hault.cm@gmail.com>
2023-03-09 23:45:29 +0700

    fix lỗi hiển thị

--- voting/views.py
+++ voting/views.py
@@ -25,75 +25,73 @@ def generate_ballot(display_controls=False):
     output = ""
     candidates_data = ""
     num = 1
+    instruction = ""
     # return None
     hide = 0
-    for position in positions:        
-        if positions.count() == hide:  
-            output='<div class="text-center"><div class="col-xs-12"><div class="box box-solid"><div class="box-header with-border"><h3 class="box-title"><b>Tạm thời chưa có bình chọn nào!</b></h3></div><div class="box-body"><p>Vui lòng đợi trong giây lát!</p></div> <div ><button type="button" id="refresh" class="btn btn-success btn-sm btn-flat reset" data-desc="{position_name}"><i class="fa fa-refresh"></i> Làm mới</button></div></div></div></div>'
-            break
-           
-        name = position.name
-        position_name = slugify(name)
-        candidates = Candidate.objects.filter(position=position)
-        for candidate in candidates:
-            
-            if position.max_vote > 1:
-                instruction = "Bạn có thể chọn tối đa " + \
-                    str(position.max_vote) + " đại biểu"
-                input_box = '<input type="checkbox" value="'+str(candidate.id)+'" class="flat-red ' + \
-                    position_name+'" name="' + \
-                    position_name+"[]" + '">'
-                
-            else:
-                if position.max_vote < 1:
-                    hide+=1
-                    output=""
+    for position in positions:                
+        if position.max_vote < 1:
+            hide+=1
+        else:
+            name = position.name
+            position_name = slugify(name)
+            candidates = Candidate.objects.filter(position=position)
+            for candidate in candidates:  
+                if position.max_vote > 1:
+                    instruction = "Bạn có thể chọn tối đa " + \
+                        str(position.max_vote) + " đại biểu"
+                    input_box = '<input type="checkbox" value="'+str(candidate.id)+'" class="flat-red ' + \
+                        position_name+'" name="' + \
+                        position_name+"[]" + '">'
+                    
                 else:
-                    instruction = "Chỉ chọn một đại biểu " + str(hide)
+                    instruction = "Chỉ chọn một đại biểu "  
                     input_box = '<input value="'+str(candidate.id)+'" type="radio" class="flat-red ' + \
                         position_name+'" name="'+position_name+'">'
                 image = "/media/" + str(candidate.photo)
                 candidates_data = candidates_data + '<li>' + input_box + '<button type="button" class="btn btn-primary btn-sm btn-flat clist platform" data-fullname="'+candidate.fullname+'" data-bio="'+candidate.bio+'"><i class="fa fa-search"></i> Chi tiết</button><img src="' + \
                     image+'" height="100px" width="100px" class="clist"><span class="cname clist">' + \
-                    candidate.fullname+'</span></li>'
-        up = ''
-        if position.priority == 1:
-            up = 'disabled'
-        down = ''
+                        candidate.fullname+'</span></li>'
+            up = ''
+            if position.priority == 1:
+                up = 'disabled'
+            down = ''
 
-        if position.priority == (positions.count()-hide):
-            down = 'disabled'
-        output = output + f"""<div class="row">	<div class="col-xs-12"><div class="box box-solid" id="{position.id}">
-             <div class="box-header with-border">
-            <h3 class="box-title"><b>{name}</b></h3>"""
+            if position.priority == (positions.count()-hide):
+                down = 'disabled'
+            output = output + f"""<div class="row">	<div class="col-xs-12"><div class="box box-solid" id="{position.id}">
+                <div class="box-header with-border">
+                <h3 class="box-title"><b>{name}</b></h3>"""
 
-        if display_controls:
-            output = output + f""" <div class="pull-right box-tools">
-        <button type="button" class="btn btn-default btn-sm moveup" data-id="{position.id}" {up}><i class="fa fa-arrow-up"></i> </button>
-        <button type="button" class="btn btn-default btn-sm movedown" data-id="{position.id}" {down}><i class="fa fa-arrow-down"></i></button>
-        </div>"""
+            if display_controls:
+                output = output + f""" <div class="pull-right box-tools">
+            <button type="button" class="btn btn-default btn-sm moveup" data-id="{position.id}" {up}><i class="fa fa-arrow-up"></i> </button>
+            <button type="button" class="btn btn-default btn-sm movedown" data-id="{position.id}" {down}><i class="fa fa-arrow-down"></i></button>
+            </div>"""
 
-        output = output + f"""</div>
-        <div class="box-body">
-        <p>{instruction}
-        <span class="pull-right">
-        <button type="button" class="btn btn-success btn-sm btn-flat reset" data-desc="{position_name}"><i class="fa fa-refresh"></i> Làm mới</button>
-        </span>
-        </p>
-        <div id="candidate_list">
-        <ul>
-        {candidates_data}
-        </ul>
-        </div>
-        </div>
-        </div>
-        </div>
-        </div>
-        """
-        position.priority = num
-        position.save()
-        num = num + 1
-        candidates_data = ''
+            output = output + f"""</div>
+            <div class="box-body">
+            <p>{instruction}
+            <span class="pull-right">
+            <button type="button" class="btn btn-success btn-sm btn-flat reset" data-desc="{position_name}"><i class="fa fa-refresh"></i> Làm mới</button>
+            </span>
+            </p>
+            <div id="candidate_list">
+            <ul>
+            {candidates_data}
+            </ul>
+            </div>
+            </div>
+            </div>
+            </div>
+            </div>
+            """
+            position.priority = num
+            position.save()
+            num = num + 1
+            candidates_data = ''
+        if positions.count() == hide:  
+            output= '<div class="text-center"><div class="col-xs-12"><div class="box box-solid"><div class="box-header with-border"><h3 class="box-title"><b>Tạm thời chưa có bình chọn nào!</b></h3></div><div class="box-body"><p>Vui lòng đợi trong giây lát!</p></div> <div ><button type="button" id="refresh" class="btn btn-success btn-sm btn-flat reset" data-desc="{position_name}"><i class="fa fa-refresh"></i> Làm mới</button></div></div></div></div>' 
+            break
     return output
 
 
